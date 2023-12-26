1baf2affac4401253669c103c9a1e9417aa35142
KrisAirdancer <chris.scott.marston@gmail.com>
2023-12-26 13:57:30 -0700

    Added script and data files to generate post cards on homepage.

---
+++ post-data.js
@@ -0,0 +1,26 @@
+let postData = [
+    {
+        title: "Post 0",
+        blurb: "Lorem ipsum sit dolor amet.",
+        imagePath: "./img/post-1/jp-honeymoon-1.jpg",
+        postURL: "./posts/post-1.html",
+        date: "May 13, 2020",
+        author: "Chris"
+    },
+    {
+        title: "Post 1",
+        blurb: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, neque!",
+        imagePath: "./img/post-1/jp-honeymoon-1.jpg",
+        postURL: "./posts/post-1.html",
+        date: "May 13, 2020",
+        author: "Chris"
+    },
+    {
+        title: "Post 2 - Lorem!",
+        blurb: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste esse nesciunt dolore accusantium quos impedit quidem nobis dolor sunt nostrum?",
+        imagePath: "./img/post-1/jp-honeymoon-1.jpg",
+        postURL: "./posts/post-1.html",
+        date: "May 13, 2020",
+        author: "Chris"
+    }
+]
