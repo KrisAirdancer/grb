b148c492f54201421ad478854f3eb2802a3a080d
KrisAirdancer <chris.scott.marston@gmail.com>
2023-12-26 14:32:23 -0700

    Added logic to generate a list of posts on the archives page.

--- post-data.js
+++ post-data.js
@@ -4,7 +4,7 @@ let postData = [
         blurb: "Lorem ipsum sit dolor amet.",
         imagePath: "./img/post-1/jp-honeymoon-1.jpg",
         postURL: "./posts/post-1.html",
-        date: "May 13, 2020",
+        date: "May 13, 2017",
         author: "Chris"
     },
     {
@@ -12,7 +12,7 @@ let postData = [
         blurb: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, neque!",
         imagePath: "./img/post-1/jp-honeymoon-1.jpg",
         postURL: "./posts/post-1.html",
-        date: "May 13, 2020",
+        date: "May 14, 2018",
         author: "Chris"
     },
     {
@@ -20,7 +20,55 @@ let postData = [
         blurb: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste esse nesciunt dolore accusantium quos impedit quidem nobis dolor sunt nostrum?",
         imagePath: "./img/post-1/jp-honeymoon-1.jpg",
         postURL: "./posts/post-1.html",
-        date: "May 13, 2020",
+        date: "May 15, 2019",
+        author: "Chris"
+    },
+    {
+        title: "Post 2 - Lorem!",
+        blurb: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste esse nesciunt dolore accusantium quos impedit quidem nobis dolor sunt nostrum?",
+        imagePath: "./img/post-1/jp-honeymoon-1.jpg",
+        postURL: "./posts/post-1.html",
+        date: "May 15, 2020",
+        author: "Chris"
+    },
+    {
+        title: "Post 2 - Lorem!",
+        blurb: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste esse nesciunt dolore accusantium quos impedit quidem nobis dolor sunt nostrum?",
+        imagePath: "./img/post-1/jp-honeymoon-1.jpg",
+        postURL: "./posts/post-1.html",
+        date: "May 15, 2021",
+        author: "Chris"
+    },
+    {
+        title: "Post 2 - Lorem!",
+        blurb: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste esse nesciunt dolore accusantium quos impedit quidem nobis dolor sunt nostrum?",
+        imagePath: "./img/post-1/jp-honeymoon-1.jpg",
+        postURL: "./posts/post-1.html",
+        date: "May 15, 2022",
+        author: "Chris"
+    },
+    {
+        title: "Post 2 - Lorem!",
+        blurb: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste esse nesciunt dolore accusantium quos impedit quidem nobis dolor sunt nostrum?",
+        imagePath: "./img/post-1/jp-honeymoon-1.jpg",
+        postURL: "./posts/post-1.html",
+        date: "June 1, 2019",
+        author: "Chris"
+    },
+    {
+        title: "Post 2 - Lorem!",
+        blurb: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste esse nesciunt dolore accusantium quos impedit quidem nobis dolor sunt nostrum?",
+        imagePath: "./img/post-1/jp-honeymoon-1.jpg",
+        postURL: "./posts/post-1.html",
+        date: "December 24, 2019",
+        author: "Chris"
+    },
+    {
+        title: "Post 2 - Lorem!",
+        blurb: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste esse nesciunt dolore accusantium quos impedit quidem nobis dolor sunt nostrum?",
+        imagePath: "./img/post-1/jp-honeymoon-1.jpg",
+        postURL: "./posts/post-1.html",
+        date: "August 18, 2022",
         author: "Chris"
     }
 ]
